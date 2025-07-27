61ffe9e5c72cdefe6e13d97c44f316d9a05d06f4
ravshan <ravshan.personal@gmail.com>
2025-07-27 17:03:20 +0600

    added python support

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -32,7 +32,7 @@ return { -- Autoformat
       lua = { 'stylua' },
       go = { 'goimports', 'gofumpt' },
       -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
+      -- python = { 'isort', 'black' },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
